40e9d7a2cfd828de5f220f1db0c64058028a95b2
DH-TB <929659475@qq.com>
2017-06-14 17:25:18 +0800

    add event

--- ws/client.js
+++ ws/client.js
@@ -6,6 +6,10 @@ ws.on('open', function open() {
     ws.send('something1');
 });
 
+ws.addEventListener('open', function (event) {
+    ws.send('Hello Server!');
+});
+
 ws.on('message', function incoming(data) {
     console.log(data);
 });
